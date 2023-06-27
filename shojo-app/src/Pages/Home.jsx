2fce95ec95938d1bb023521967903077491f7b3e
yuyu-10 <104772989+yuyu-10@users.noreply.github.com>
2023-06-28 00:44:08 +0200

    refonte de la page Home, follow du design créé

--- shojo-app/src/Pages/Home.jsx
+++ shojo-app/src/Pages/Home.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import logo from '../img/logo_TK.png'
+import bandes_blanches from '../img/bandes_blanches.png'
+import book from '../img/manga.png'
 import { useNavigate } from "react-router-dom"
-import { Zoom } from "react-awesome-reveal";
+// import { Zoom } from "react-awesome-reveal";
 import '../css/Home.css'
 
 const Home = () => {
@@ -9,24 +10,54 @@ const Home = () => {
     const [isHovered, setIsHovered] = useState(false);
 
     return (
-        <div style={{backgroundColor: "#EDDBC6", height: '100vh', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <Zoom triggerOnce>
-                <img
+        <div className='backHome'>
+            <div className='intro'>
+                <p className='titleHome'>TOSHO TO KAN</p>
+                <div className='slogan'>
+                    <p>Ta library de shojo</p>
+                    <p>en un click !</p>
+                </div>
+                <img className="imageBandes" src={bandes_blanches}/>
+                <button
+                    className='startButton'
                     style={{
-                    width: "40vh",
-                    height: "40vh",
-                    transform: isHovered ? "scale(1.2)" : "scale(1)",
-                    transition: "transform 0.3s ease"
+                        transform: isHovered ? "scale(1.2)" : "scale(1)",
+                        transition: "transform 0.3s ease"
                     }}
                     onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}
                     onClick={() => navigate("/liste")}
-                    src={logo}
-                    alt=""
-                />
-            </Zoom>
-        </div >
+                >
+                    Entrer
+                </button>
+            </div>
+            <img
+                className="book"
+                src={book}
+                style={{
+                    cursor: 'default'
+                }}
+            />
+        </div>
     )
 }
 
 export default Home
+
+// <div style={{backgroundColor: "#EDDBC6", height: '100vh', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+//     <Zoom triggerOnce>
+//         <img
+//             style={{
+//             width: "40vh",
+//             height: "40vh",
+            // transform: isHovered ? "scale(1.2)" : "scale(1)",
+            // transition: "transform 0.3s ease"
+//             }}
+//             onMouseEnter={() => setIsHovered(true)}
+//             onMouseLeave={() => setIsHovered(false)}
+//             onClick={() => navigate("/liste")}
+//             src={logo}
+//             alt=""
+//         />
+//     </Zoom>
+// </div >
